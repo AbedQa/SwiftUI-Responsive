ebb100c78a7cb6f6cc0699e2649c0b4770be604c
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2022-03-12 21:04:56 +0300

    Initial Commit

---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by AbdelrahmanQasim on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
